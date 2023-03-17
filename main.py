2a2dea7dbafb8336794278d9ac38b0cb9332a217
David <41566942+dvendt@users.noreply.github.com>
2023-03-17 14:55:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def hello():
 @app.route('/echo/<name>')
 def echo(name):
     print(f"this was placed in the url: {name}")
-    val = {"new-name2": name}
+    val = {"new-name4": name}
     return jsonify(val)
     
 if __name__ == '__main__':
